16c85b35e7c5cf5cc9dcd9827b65c565a405e243
Imartin4 <imartin4@mail.pima.edu>
2024-09-25 06:06:05 -0700

    Update Cis129_lab02_shortstory.py

--- Cis129_lab02_shortstory.py
+++ Cis129_lab02_shortstory.py
@@ -1,1 +1,2 @@
-print("Hello, World!") #First code 
+#Author:Isaiah Martin # My first Python lab in cis129 # Prints simple short story 
+print(" When I started this class, I was excited. I'm veen more excited that were are doing this on Github. I will undertsand Github.") 
